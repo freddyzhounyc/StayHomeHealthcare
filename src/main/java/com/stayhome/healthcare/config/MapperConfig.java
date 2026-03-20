9afbaa0b75a04ca36a5d2ff89ae89ceca0ba0549
Freddy Zhou <freddyzhou06@gmail.com>
2026-03-19 23:50:54 -0400

    Add TODO comment

--- src/main/java/com/stayhome/healthcare/config/MapperConfig.java
+++ src/main/java/com/stayhome/healthcare/config/MapperConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperConfig {
 
+    // TODO: Update to use ModelStruct instead
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
